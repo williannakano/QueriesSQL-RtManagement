f92a4fbf2eceacef827abffbf012def4e35c69bf
Willian Nakano <95592340+williannakano@users.noreply.github.com>
2025-01-24 19:24:26 -0300

    Update Atividades.sql

--- Queries/Atividades.sql
+++ Queries/Atividades.sql
@@ -1,63 +1,64 @@
 with
 BS_ATIVIDADES AS (
-		SELECT
-				*
-		FROM
-				(
-						select
-								 AT.DimPatientID
-								,IIF(LEFT(AC.ActivityCategoryCode,2)='00','Tratamento', 'Planning') as TIP_DADO
-								,AC.ActivityCategoryCode	AS RECURSO_CATEGORIA
-								,CASE 
+SELECT
+	*
+FROM
+	(
+	select
+			 AT.DimPatientID
+			,IIF(LEFT(AC.ActivityCategoryCode,2)='00','Tratamento', 'Planning') as TIP_DADO
+			,AC.ActivityCategoryCode	AS RECURSO_CATEGORIA
+			,CASE 
+				WHEN AC.ActivityCode = 'Conferir ficha' THEN 'Duplo Check'	
+					WHEN AC.ActivityCode = 'Preparar tudo' THEN 'Montar ficha'	
+						WHEN AC.ActivityCode = 'Planejamento URG' THEN 'Planejamento'
+							WHEN AC.ActivityCode = 'Aprovar plano URG' THEN 'Aprovar plano'
+								WHEN AC.ActivityCode = 'Contorno URG' THEN 'Contornar Estruturas'
+					else AC.ActivityCode
+				end AS NOM_ATIVIDADE_2
+			,CASE 
+				WHEN AC.ActivityCode = 'Conferir ficha' THEN 'Duplo Check'	
+					WHEN AC.ActivityCode = 'Preparar tudo' THEN 'Montar ficha'	
+					else AC.ActivityCode
+				end AS NOM_ATIVIDADE
+			,US1.UserId					AS ID_RECURSO_ATENDIMENTO
+			,US1.DisplayName			AS NOM_RECURSO
+			,AC.ctrActivityCategorySer
+			,AC.ctrActivitySer
+			,AT.AppointmentStatus				AS STATUS
+			,case 
+				when AC.ActivityCode like '%URG%' then 'URGENTE'
+					when AC.ActivityCode like '%2D%' then 'ELETRONS'
+						ELSE 'CONVENCIONAL'
+				END AS AUX_TIP_FLUXO
+			,AppointmentDateTime as data_agendamento_atv
+			,ScheduledEndTime as data_vencimento_agendamento
+			,AT.ActivityStartDateTime AS DATA_INI_ATIVIDADE
+			,AT.ActivityEndDateTime as DATA_FIM_ATIVIDADE
+			,ROW_NUMBER() OVER (PARTITION BY AT.DimPatientID, (
+									CASE 
 									WHEN AC.ActivityCode = 'Conferir ficha' THEN 'Duplo Check'	
-										WHEN AC.ActivityCode = 'Preparar tudo' THEN 'Montar ficha'	
-											WHEN AC.ActivityCode = 'Planejamento URG' THEN 'Planejamento'
-												WHEN AC.ActivityCode = 'Aprovar plano URG' THEN 'Aprovar plano'
-													WHEN AC.ActivityCode = 'Contorno URG' THEN 'Contornar Estruturas'
-										else AC.ActivityCode
-									end AS NOM_ATIVIDADE_2
-								,CASE 
-									WHEN AC.ActivityCode = 'Conferir ficha' THEN 'Duplo Check'	
-										WHEN AC.ActivityCode = 'Preparar tudo' THEN 'Montar ficha'	
-										else AC.ActivityCode
-									end AS NOM_ATIVIDADE
-								,US1.UserId					AS ID_RECURSO_ATENDIMENTO
-								,US1.DisplayName			AS NOM_RECURSO
-								,AC.ctrActivityCategorySer
-								,AC.ctrActivitySer
-								,AT.AppointmentStatus				AS STATUS
-								,case 
-									when AC.ActivityCode like '%URG%' then 'URGENTE'
-										when AC.ActivityCode like '%2D%' then 'ELETRONS'
-											ELSE 'CONVENCIONAL'
-									END AS AUX_TIP_FLUXO
-								,AppointmentDateTime as data_agendamento_atv
-								,ScheduledEndTime as data_vencimento_agendamento
-								,AT.ActivityStartDateTime AS DATA_INI_ATIVIDADE
-								,AT.ActivityEndDateTime as DATA_FIM_ATIVIDADE
-								,ROW_NUMBER() OVER (PARTITION BY AT.DimPatientID, (CASE 
-																						WHEN AC.ActivityCode = 'Conferir ficha' THEN 'Duplo Check'	
-																								WHEN AC.ActivityCode = 'Preparar tudo' THEN 'Montar ficha'	
-																									WHEN AC.ActivityCode = 'Planejamento URG' THEN 'Planejamento'
-																										WHEN AC.ActivityCode = 'Aprovar plano URG' THEN 'Aprovar plano'
-																											WHEN AC.ActivityCode = 'Contorno URG' THEN 'Contornar Estruturas'
-																								else AC.ActivityCode
-																							end) ORDER BY ScheduledEndTime ASC) NUM_FLUXO
-								,CASE 
-									WHEN AC.ActivityCode = 'Solicitacao TC' THEN 1
-										ELSE AT.ActivityOwnerFlag
-									END AS FLG_1
-						from
-											[variandw].[DWH].[DimActivityTransaction]	as AT (NOLOCK)
-								INNER JOIN	[variandw].[DWH].[DimActivity]				as AC (NOLOCK) ON AC.DimActivityID = AT.DimActivityID
-								LEFT  JOIN	[variandw].[DWH].[DimUser]					as US1 (NOLOCK) ON US1.DimResourceID = AT.DimResourceID
-						where
-									1=1
-								AND AppointmentResourceStatus NOT IN ('Deleted')
-								and AppointmentStatus NOT IN ('Cancelled')
-								AND AT.DimResourceID <> 0 
-								and cast(AT.ActivityEndDateTime as date) >= '2023-01-01'
-				) AS BS
+									WHEN AC.ActivityCode = 'Preparar tudo' THEN 'Montar ficha'	
+									WHEN AC.ActivityCode = 'Planejamento URG' THEN 'Planejamento'
+									WHEN AC.ActivityCode = 'Aprovar plano URG' THEN 'Aprovar plano'
+									WHEN AC.ActivityCode = 'Contorno URG' THEN 'Contornar Estruturas'
+									ELSE AC.ActivityCode
+									END) ORDER BY ScheduledEndTime ASC) NUM_FLUXO
+			,CASE 
+				WHEN AC.ActivityCode = 'Solicitacao TC' THEN 1
+					ELSE AT.ActivityOwnerFlag
+				END AS FLG_1
+	from
+						[variandw].[DWH].[DimActivityTransaction]	as AT (NOLOCK)
+			INNER JOIN	[variandw].[DWH].[DimActivity]				as AC (NOLOCK) ON AC.DimActivityID = AT.DimActivityID
+			LEFT  JOIN	[variandw].[DWH].[DimUser]				as US1 (NOLOCK) ON US1.DimResourceID = AT.DimResourceID
+	where
+				1=1
+			AND AppointmentResourceStatus NOT IN ('Deleted')
+			and AppointmentStatus NOT IN ('Cancelled')
+			AND AT.DimResourceID <> 0 
+			and cast(AT.ActivityEndDateTime as date) >= '2023-01-01'
+	) AS BS
 	),
 BS_ELETRONS_URG AS (
 		SELECT distinct 
