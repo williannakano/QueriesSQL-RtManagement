c0391b42a1c2071e447633438a2ab293bd9417c5
Willian Nakano <95592340+williannakano@users.noreply.github.com>
2025-01-24 19:27:22 -0300

    Update Paciente.sql

--- Queries/Paciente.sql
+++ Queries/Paciente.sql
@@ -1,14 +1,13 @@
 SELECT DISTINCT
-		 DimPatientID
-		,PatientId
-		,PatientId2
-		,PatientFullName
-		,PatientDateOfBirth
-		,PatientType
-		,HstryDateTime
-		,PatientDeathStatus
-		,DR.DoctorFullName
+	 DimPatientID
+	,PatientId
+	,PatientId2
+	,PatientFullName
+	,PatientDateOfBirth
+	,PatientType
+	,HstryDateTime
+	,PatientDeathStatus
+	,DR.DoctorFullName
 FROM 
-					[variandw].[DWH].[DimPatient] AS PT (NOLOCK)
-		LEFT JOIN	[variandw].[DWH].[DimDoctor]  AS DR (NOLOCK) ON DR.ctrResourceSer = PT.ctrPrimaryOncologistSer
---order by 1 desc;
+		  [variandw].[DWH].[DimPatient] AS PT (NOLOCK)
+	LEFT JOIN [variandw].[DWH].[DimDoctor]  AS DR (NOLOCK) ON DR.ctrResourceSer = PT.ctrPrimaryOncologistSer
